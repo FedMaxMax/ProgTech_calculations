a48afd810e9844add00d8789e5902e6f9ca6babb
Maxim Fedotov 21312 <maxfm2003@mail.ru>
2023-11-10 14:01:10 +0300

    added the skeleton of the program

---
+++ calculation/calculation/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include "funcs.h"
+
+int main()
+{
+
+std::string text;
+double num1, num2;
+char choice;
+
+std::cout << "Введите первое число:";
+std::cin >> text;
+num1 = strtod(text.c_str(),NULL);
+std::cout << "Введите второе число:";
+std::cin >> text;
+num2 = strtod(text.c_str(),NULL);
+std::cout << "Выберите действие.\n1 - сложение.\n2 - вычитание.\n3 - умножение.\n4 - деление.\n";
+std::cout << "Напишите 1, 2, 3 или 4 и нажмите Enter:";
+std::cin >> text;
+choice = strtod(text.c_str(),NULL);
+switch (choice)
+    {
+    case 1:
+        std::cout << "Функция в разработке.";
+        break;
+    case 2:
+        std::cout << "Функция в разработке.";
+        break;
+    case 3:
+        std::cout << "Функция в разработке.";
+        break;
+    case 4:
+        std::cout << "Функция в разработке.";
+        break;
+    default:
+        std::cout << "Нет такой команды.";
+    }
+std::cin.get();
+return 0;
+}
+
